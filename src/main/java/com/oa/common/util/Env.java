f9a932cf6680091f5a00b7d802e60bf3731daee6
wenjie.wu-cc@outlook.com <wenjie.wu-cc@outlook.com>
2018-08-23 11:50:54 +0800

    添加排除人员过滤

---
+++ src/main/java/com/oa/common/util/Env.java
@@ -0,0 +1,12 @@
+package com.oa.common.util;
+
+public class Env {
+    public static final String CORP_ID = "dingc5cc1be25c58c8da";
+    public static final String CORP_SECRET = "9QHrT1ejb2hLA9TKI3TFHpBQrhqZImX-qEsnhyjRDOuXAHxapkNBn61F725NjpGs";
+    public static final String SSO_Secret = "wSXK3RbbUeryRi3zaqHQ0APatwhSJ1XFtKZ3gNiyoZOHMFTktVLaG8aYBnHjGAdm";
+    public static final String agent_id = "176855488";
+    public static final String OAPI_HOST = "https://oapi.dingtalk.com";
+    public static final String OA_BACKGROUND_URL = "http://localhost:8080/";
+    public static final String TOKEN = "";
+    public static final String ENCODING_AES_KEY = "";
+}
